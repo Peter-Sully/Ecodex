e21edbffc5cd5afa937e60a51d33b357f46cb68d
Conor Floyd <cfloyd@hmc.edu>
2023-04-15 13:44:07 -0700

    poaksd

--- infoFromImage.py
+++ infoFromImage.py
@@ -5,17 +5,12 @@ from pprint import pprint
 def sciNameFromImage(image_data) -> str:
     API_KEY = "2b10uneIXKpVTSEFhRzryEJ6W"  # our API key
     api_endpoint = f"https://my-api.plantnet.org/v2/identify/all?api-key={API_KEY}"
-
     files = [('images', ("image_path", image_data))]
-
     req = requests.post(api_endpoint, files=files)
-
     json_result = json.loads(req.text)
     
     if req.status_code != 200:
         print("bad things happened, image recog API not 200 status code")
-    # print(req.status_code)
-    # print("json result:", json_result)
     
     return json_result["results"][0]["species"]["scientificNameWithoutAuthor"]
 
@@ -41,13 +36,9 @@ def infoFromsciName(sciName : str):
         "classificationOptions": None,
         "speciesTaxonomyCriteria": []
     }
-    # print(payload)
     response = requests.post(api_url+"speciesSearch", json=payload)
     uniqueId = response.json()["results"][0]["uniqueId"]
-    # print(response.status_code)
-    # print(response.json())
-    # print(uniqueId)
-
+    
     response = requests.post(api_url+"taxon/"+uniqueId)
 
     return response.json()
@@ -56,17 +47,12 @@ def infoFromImage(image_data):
     sciName = sciNameFromImage(image_data) #accesses Pl@ntNet API to recognize image
     infoJson = infoFromsciName(sciName) #accesses NatureServe Explorer REST API to get information about plant
     return infoJson
-    
 
 # sciName = sciNameFromImage("./data/image_1.jpeg")
 # sciName = "Epilobium canum"
-
 # plantdata = infoFromsciName(sciName)
-
 # print("plant data:",plantdata)
-
 
 with open("./data/image_1.jpeg", 'rb') as image_data:
     theData = infoFromImage(image_data)
 
-print(theData)
